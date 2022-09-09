640965b0011e35c70f47995c5e72e7ac9ae681b2
Sérgio Lemos <contato@sergiolemos.com>
2022-09-09 13:18:00 -0300

    header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import Header from "./components/Header";
 import NotificationButton from "./components/NotificationButton";
 
 function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
+      <Header />
       <NotificationButton />
     </>
   )
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className='dsmeta-logo-container'>
+                <img src={logo} alt="DS META" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvimento por <a href="https://www.instagram.com/devsuperior.ig" target='blank'>@devsuperior.ig</a></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
